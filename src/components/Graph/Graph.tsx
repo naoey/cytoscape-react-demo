28fe3b9d7d219db003e369660d34af4b8f125973
Potla1995 <sayanmukherjee1995@gmail.com>
2023-03-28 08:52:25 +0900

    Partially implement adding nodes

--- src/components/Graph/Graph.tsx
+++ src/components/Graph/Graph.tsx
@@ -1,35 +1,63 @@
 import styled from "styled-components";
 import cytoscape from "cytoscape";
-import { useEffect, useRef } from "react";
-import { v4 as uuid } from "uuid";
+import {Component, Fragment} from "react";
+import {v4 as uuid} from "uuid";
+import {Button} from "react-bootstrap";
 
-const StyledGraphWrapper = styled.div`
-  height: 100%;
-  width: 100%;
+const StyledGraphWrapper = styled.div`{
+  height: 70%;
+  width: 70%;
   background-color: grey;
-`;
+}`;
 
-export default function Graph() {
-  const cyRef = useRef<any>(null);
-  const wrapperRef = useRef(null);
+class Graph extends Component<{},{addNode: boolean, connectEdge: boolean, cy: cytoscape.Core}>{
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      addNode: false,
+      connectEdge: false,
+      cy: cytoscape(),
+    };
+  }
 
-  useEffect(() => {
-    cyRef.current = cytoscape({ container: wrapperRef.current });
-  }, []);
-
-  const onCanvasClick = (e: React.MouseEvent<HTMLDivElement>) => {
+  onCanvasClick = (e: React.MouseEvent<HTMLDivElement>) => {
     const position = { x: e.clientX, y: e.clientY };
+    // const button = e.button
+    if(this.state.addNode) {
+      console.log("Adding node at position", position);
 
-    console.log("Adding node at position", position);
-
-    cyRef.current.add([
-      { group: "nodes", data: { id: uuid() }, renderedPosition: position },
-    ]);
+      this.state.cy.add([
+        {group: "nodes", data: {id: uuid()}, renderedPosition: position},
+      ]);
+    }
   };
 
-  return (
-    <StyledGraphWrapper id="cy" ref={wrapperRef} onClick={onCanvasClick}>
-      <h1>hello world</h1>
-    </StyledGraphWrapper>
-  );
+  onAddNodeClick = () => {
+    console.log("Button Add Node Clicked!");
+    this.setState({addNode: !this.state.addNode});
+  }
+
+  onConnectEdgeClick = () => {
+    console.log("Button Connect Edge Clicked!");
+    this.setState({addNode: false, connectEdge: !this.state.connectEdge});
+  }
+
+  render() {
+    return (
+        <Fragment>
+          <StyledGraphWrapper id="cy" onClick={this.onCanvasClick}/>
+          <div>
+          <Button onClick={this.onAddNodeClick} style={{color: this.state.addNode ? "#00F" : "#F00"}}>
+            Add Node
+          </Button>
+          <Button onClick={this.onConnectEdgeClick} style={{color: this.state.connectEdge ? "#00F" : "#F00"}}>
+            Connect Edge
+          </Button>
+          </div>
+          <h1>Graph-GUI</h1>
+        </Fragment>
+    );
+  }
+
 }
+export default Graph
