c57471508e4225f264be89e26a038f29660996e3
Potla1995 <sayanmukherjee1995@gmail.com>
2023-03-29 16:43:37 +0900

    Implement adding nodes with automatic labels

--- src/components/Graph/Graph.tsx
+++ src/components/Graph/Graph.tsx
@@ -1,13 +1,13 @@
 import styled from "styled-components";
 import cytoscape from "cytoscape";
 import { useEffect, useRef, useState } from "react";
-import { v4 as uuid } from "uuid";
+// import { v4 as uuid } from "uuid";
 import { Button } from "react-bootstrap";
 
 const StyledGraphWrapper = styled.div`
   height: 70%;
   width: 70%;
-  background-color: grey;
+  background-color: lightgray;
 `;
 
 export default function Graph() {
@@ -15,22 +15,37 @@ export default function Graph() {
     isAddEnabled: false,
     shouldConnectEdge: false,
   });
+  const [latestVertexID, setLatestVertexID] = useState<number>(1);
   const wrapperRef = useRef<HTMLDivElement>(null);
   const cyRef = useRef<any>(null);
 
   useEffect(() => {
-    cyRef.current = cytoscape({ container: wrapperRef.current });
+    cyRef.current = cytoscape({
+      container: wrapperRef.current,
+      style: [{
+        selector: 'node',
+        css: {
+          'label': 'data(id)',
+          'text-valign': 'center',
+        }
+      }],
+    });
   }, []);
 
   const onCanvasClick = (e: React.MouseEvent<HTMLDivElement>) => {
     const position = { x: e.clientX, y: e.clientY };
-    // const button = e.button
-    if (controls.isAddEnabled) {
+    const button = e.button
+    if (button === 2) {
+      console.log("Right button clicked at position", position);
+      // Open context menu
+    }
+    else if (button === 0 && controls.isAddEnabled) {
       console.log("Adding node at position", position);
-
+      setLatestVertexID(latestVertexID+1)
       cyRef.current.add([
-        { group: "nodes", data: { id: uuid() }, renderedPosition: position },
+        { group: "nodes", data: { id: latestVertexID }, renderedPosition: position },
       ]);
+      console.log("Current nodes list: ", cyRef.current.nodes().map((e: { id: () => any; })=>e.id()))
     }
   };
 
@@ -42,8 +57,8 @@ export default function Graph() {
 
   const onAddClick = () =>
     setControls((controls) => ({
-      ...controls,
       isAddEnabled: !controls.isAddEnabled,
+      shouldConnectEdge: false,
     }));
 
   return (
